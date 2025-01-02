7758d88d507a6e5c62b5dfd40ff74c3ce94f6793
Padma Leka T <113542308+padmaleka@users.noreply.github.com>
2025-01-02 17:55:08 +0530

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int n = 1;
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()){
+            String a = scan.nextLine();
+            System.out.println(n + " "+a);
+            n++;
+        } 
+    }
+}
